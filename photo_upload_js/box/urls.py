b6bda5fd34faa039021ae70fe67fbca8ed4db2e2
nilldiggonto <nilldiggonto@gmail.com>
2021-02-09 22:18:54 +0600

    pic uploaderA

--- photo_upload_js/box/urls.py
+++ photo_upload_js/box/urls.py
@@ -1,9 +1,10 @@
-from .views import MainView
+from .views import MainView,image_upload_view
 from django.urls import path
 
 
 urlpatterns = [
     path('',MainView.as_view(),name='index'),
+    path('upload/',image_upload_view,name='upload'),
 ]
 
 
--- photo_upload_js/box/views.py
+++ photo_upload_js/box/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
 from django.views.generic import TemplateView
+from .models import BoxforImage
 # Create your views here.
 
 class MainView(TemplateView):
     template_name = 'box/index.html'
+
+
+def image_upload_view(request):
+    print('abc')
+    if request.method == 'POST':
+        box_file = request.FILES.get('file')
+        BoxforImage.objects.create(upload=box_file)
+        return HttpResponse('upload')
+    return JsonResponse({'get':'not allowed'})
+    # return HttpResponse('upload')
+
