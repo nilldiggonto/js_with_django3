f13e84d5f3bf5b91d27d51130607515325fc4a57
nilldiggonto <nilldiggonto@gmail.com>
2021-02-10 12:04:18 +0600

    url template

--- photo_upload_js/photo_upload_js/urls.py
+++ photo_upload_js/photo_upload_js/urls.py
@@ -6,6 +6,7 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('box/',include('box.urls')),
+    path('search/',include('search.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
--- photo_upload_js/search/views.py
+++ photo_upload_js/search/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import TestSearch
 # Create your views here.
+from django.views.generic import ListView
+
+class SearchListView(ListView):
+    model = TestSearch
+    template_name = 'search/list_item.html'
