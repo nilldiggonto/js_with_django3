1160a6775bb894479d2ac308d60559c8e630bfb2
nilldiggonto <nilldiggonto@gmail.com>
2021-02-10 12:45:14 +0600

    JS Search

--- photo_upload_js/search/views.py
+++ photo_upload_js/search/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 from .models import TestSearch
 # Create your views here.
 from django.views.generic import ListView
+import json
 
 class SearchListView(ListView):
     model = TestSearch
     template_name = 'search/list_item.html'
+
+    def get_context_data(self,*args,**Kwargs):
+        context = super().get_context_data(**Kwargs)
+        context['json_data']= json.dumps(list(TestSearch.objects.values()))
+        return context
