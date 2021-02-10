11967c2e3da99b6436e42d6c9e955091dbe6a515
nilldiggonto <nilldiggonto@gmail.com>
2021-02-10 11:45:15 +0600

    Model for Search

--- photo_upload_js/photo_upload_js/settings.py
+++ photo_upload_js/photo_upload_js/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'box',
+    'search',
 ]
 
 MIDDLEWARE = [
--- photo_upload_js/search/admin.py
+++ photo_upload_js/search/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import TestSearch
 # Register your models here.
+admin.site.register(TestSearch)
